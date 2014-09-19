de14b428ba2125343d2e29618eeda68a24929da4
Christopher Moore <christopher.e.moore@gmail.com>
2014-09-19 01:23:23 -0700

    made getdata to explode elements of asteroids.  much bigger than text.  what was I thinking?  now to convert the files to something smaller...

--- jpl-ssd-smallbody/getdata.lua
+++ jpl-ssd-smallbody/getdata.lua
@@ -1,5 +1,4 @@
 #!/usr/bin/env lua -lluarocks.require
--- 
 require 'ext'
 local julian = assert(loadfile('../lua/julian.lua'))()	-- in ../lua/julian.lua
 local json = require 'dkjson'
@@ -75,123 +74,116 @@ args:
 	inputFile (text)
 	outputFile (json)
 --]]
-local function convertFile(args)
-end
+local function processFile(args)
+	local inputFilename = assert(args.inputFilename)
+	local outputFilename = assert(args.outputFilename)
+	local variableName = assert(args.variableName)
+	local processRow = assert(args.processRow)
 
--- process comets
-do
-	local lines = io.readfile('ELEMENTS.COMET'):split('\n')
+	local lastTime = os.time()
+	local lines = io.readfile(inputFilename):split('\n')
 	local cols = Columns(lines)
 	local outputLines = table()
-	outputLines:insert('cometData = [')
+	outputLines:insert(variableName..' = [')
 	for i=3,#lines do	-- skip headers and ---- line
 		local line = lines[i]
 		if #line:trim() > 0 then
 			xpcall(function()
-				local body = {}
 				local row = cols(line)
-				local numberAndName = row['Num  Name']
-				body.idNumber = numberAndName:sub(1,4):trim()
-				body.name = numberAndName:sub(6,43):trim()
-				body.epoch = assert(tonumber(row.Epoch:trim()))
-				body.perihelionDistance = assert(tonumber(row.q:trim())) * auInM
-				body.eccentricity = assert(tonumber(row.e:trim()))
-				body.inclination = math.rad(assert(tonumber(row.i:trim())))	-- wrt J2000 ecliptic plane
-				body.argumentOfPerihelion = math.rad(assert(tonumber(row.w:trim())))
-				body.longitudeOfAscendingNode = math.rad(assert(tonumber(row.Node:trim())))
-			
-				-- time of perihelion passage
-				do
-					local str = row.Tp
-					local year = assert(tostring(str:sub(1,4)))
-					local month = assert(tostring(str:sub(5,6)))
-					local day = assert(tostring(str:sub(7)))
-					
-					-- TODO calendarToJulian() function.  this is a rough rough guess.
-					body.timeOfPerihelionPassage = julian.fromCalendar{year=year, month=month, day=day}
-				end
-				
-				body.orbitSolutionReference = row.Ref:trim()
+				local body = assert(processRow(row))
 				outputLines:insert('\t'..json.encode(body)..',')
 			end, function(err)
-				io.stderr:write('failed on line '..i..'\n')
+				io.stderr:write('failed on file '..inputFilename..' line '..i..'\n')
 				io.stderr:write(err..'\n'..debug.traceback()..'\n')
 				os.exit(1)
 			end)
 		end
+		local thisTime = os.time()
+		if thisTime ~= lastTime then
+			print(inputFilename..' '..math.floor(100*(i-2)/(#lines-2))..'% complete...') 
+			lastTime = thisTime
+		end
 	end
 	outputLines:insert('];')
-	io.writefile('comets.json', outputLines:concat('\n'))
+	io.writefile(outputFilename, outputLines:concat('\n'))
+
 end
+
+-- process comets
+processFile{
+	inputFilename = 'ELEMENTS.COMET',
+	outputFilename = 'comets.json',
+	variableName = 'cometData',
+	processRow = function(row)
+		local body = {}
+		local numberAndName = row['Num  Name']
+		body.idNumber = numberAndName:sub(1,4):trim()
+		body.name = numberAndName:sub(6,43):trim()
+		body.epoch = assert(tonumber(row.Epoch:trim()))
+		body.perihelionDistance = assert(tonumber(row.q:trim())) * auInM
+		body.eccentricity = assert(tonumber(row.e:trim()))
+		body.inclination = math.rad(assert(tonumber(row.i:trim())))	-- wrt J2000 ecliptic plane
+		body.argumentOfPerihelion = math.rad(assert(tonumber(row.w:trim())))
+		body.longitudeOfAscendingNode = math.rad(assert(tonumber(row.Node:trim())))
+	
+		-- time of perihelion passage
+		do
+			local str = row.Tp
+			local year = assert(tostring(str:sub(1,4)))
+			local month = assert(tostring(str:sub(5,6)))
+			local day = assert(tostring(str:sub(7)))
+			
+			-- TODO calendarToJulian() function.  this is a rough rough guess.
+			body.timeOfPerihelionPassage = julian.fromCalendar{year=year, month=month, day=day}
+		end
+		
+		body.orbitSolutionReference = row.Ref:trim()
+		return body
+	end,
+}
 
 -- process numbered bodies 
-do
-	local lines = io.readfile('ELEMENTS.NUMBR'):split('\n')
-	local cols = Columns(lines)
-	local outputLines = table()
-	outputLines:insert('numberedSmallBodyData = [')
-	for i=3,#lines do
-		local line = lines[i]
-		if #line:trim() > 0 then
-			xpcall(function()
-				local body = {}
-				local row = cols(line)				
-				body.idNumber = row.Num:trim()
-				body.name = row.Name:trim()
-				body.epoch = assert(tonumber(row.Epoch:trim()))
-				body.semiMajorAxis = assert(tonumber(row.a:trim()))
-				body.eccentricity = assert(tonumber(row.e:trim()))
-				body.inclindation = math.rad(assert(tonumber(row.i:trim())))
-				body.argumentOfPerihelion = math.rad(assert(tonumber(row.w:trim())))
-				body.longitudeOfAscendingNode = math.rad(assert(tonumber(row.Node:trim())))
-				body.meanAnomaly = math.rad(assert(tonumber(row.M:trim())))
-				body.absoluteMagnitude = assert(tonumber(row.H:trim()))
-				body.magnitudeSlopeParameter = assert(tonumber(row.G:trim()))
-				body.orbitSolutionReference = row.Ref:trim()
-				outputLines:insert('\t'..json.encode(body)..',')
-			end, function(err)
-				io.stderr:write('failed on line '..i..'\n')
-				io.stderr:write(err..'\n'..debug.traceback()..'\n')
-				os.exit(1)
-			end)
-		end
-	end
-	outputLines:insert('];')
-	io.writefile('smallbodies-numbered.json', outputLines:concat('\n'))
-end
+processFile{
+	inputFilename = 'ELEMENTS.NUMBR',
+	outputFilename = 'smallbodies-numbered.json',
+	variableName = 'numberedSmallBodyData',
+	processRow = function(row)
+		local body = {}
+		body.idNumber = row.Num:trim()
+		body.name = row.Name:trim()
+		body.epoch = assert(tonumber(row.Epoch:trim()))
+		body.semiMajorAxis = assert(tonumber(row.a:trim()))
+		body.eccentricity = assert(tonumber(row.e:trim()))
+		body.inclindation = math.rad(assert(tonumber(row.i:trim())))
+		body.argumentOfPerihelion = math.rad(assert(tonumber(row.w:trim())))
+		body.longitudeOfAscendingNode = math.rad(assert(tonumber(row.Node:trim())))
+		body.meanAnomaly = math.rad(assert(tonumber(row.M:trim())))
+		body.absoluteMagnitude = assert(tonumber(row.H:trim()))
+		body.magnitudeSlopeParameter = assert(tonumber(row.G:trim()))
+		body.orbitSolutionReference = row.Ref:trim()
+		return body
+	end,
+}
 
 -- process unnumbered bodies 
-do
-	local lines = io.readfile('ELEMENTS.UNNUM'):split('\n')
-	local cols = Columns(lines)
-	local outputLines = table()
-	outputLines:insert('unnumberedSmallBodyData = [')
-	for i=3,#lines do
-		local line = lines[i]
-		if #line:trim() > 0 then
-			xpcall(function()
-				local body = {}
-				local row = cols(line)				
-				body.name = row.Designation:trim()
-				body.epoch = assert(tonumber(row.Epoch:trim()))
-				body.semiMajorAxis = assert(tonumber(row.a:trim()))
-				body.eccentricity = assert(tonumber(row.e:trim()))
-				body.inclindation = math.rad(assert(tonumber(row.i:trim())))
-				body.argumentOfPerihelion = math.rad(assert(tonumber(row.w:trim())))
-				body.longitudeOfAscendingNode = math.rad(assert(tonumber(row.Node:trim())))
-				body.meanAnomaly = math.rad(assert(tonumber(row.M:trim())))
-				body.absoluteMagnitude = assert(tonumber(row.H:trim()))
-				body.magnitudeSlopeParameter = assert(tonumber(row.G:trim()))
-				body.orbitSolutionReference = row.Ref:trim()
-				outputLines:insert('\t'..json.encode(body)..',')
-			end, function(err)
-				io.stderr:write('failed on line '..i..'\n')
-				io.stderr:write(err..'\n'..debug.traceback()..'\n')
-				os.exit(1)
-			end)
-		end
-	end
-	outputLines:insert('];')
-	io.writefile('smallbodies-unnumbered.json', outputLines:concat('\n'))
-end
+processFile{
+	inputFilename = 'ELEMENTS.UNNUM',
+	outputFilename = 'smallbodies-unnumbered.json',
+	variableName = 'unnumberedSmallBodyData',
+	processRow = function(row)
+		local body = {}
+		body.name = row.Designation:trim()
+		body.epoch = assert(tonumber(row.Epoch:trim()))
+		body.semiMajorAxis = assert(tonumber(row.a:trim()))
+		body.eccentricity = assert(tonumber(row.e:trim()))
+		body.inclindation = math.rad(assert(tonumber(row.i:trim())))
+		body.argumentOfPerihelion = math.rad(assert(tonumber(row.w:trim())))
+		body.longitudeOfAscendingNode = math.rad(assert(tonumber(row.Node:trim())))
+		body.meanAnomaly = math.rad(assert(tonumber(row.M:trim())))
+		body.absoluteMagnitude = assert(tonumber(row.H:trim()))
+		body.magnitudeSlopeParameter = assert(tonumber(row.G:trim()))
+		body.orbitSolutionReference = row.Ref:trim()
+		return body
+	end,
+}
 
