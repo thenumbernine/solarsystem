079ce404edfe5cb55566a350a6e770c8e4315ab7
Christopher E. Moore <christopher.e.moore@gmail.com>
2014-09-28 00:37:46 -0700

    updated star database float buffer generation

--- hyg/getdata.lua
+++ hyg/getdata.lua
@@ -8,7 +8,8 @@ local header = lines:remove(1)
 local columnNames = header:split(',')	-- columnNames[1] = 'StarID'
 local columnForNames = columnNames:map(function(name,i) return i,name end)	-- columnForNames.StarID = 1
 
-local buffer = ffi.new('float[?]', 3*#lines)	-- allocate space for xyz of each planet 
+local numElem = 4
+local buffer = ffi.new('float[?]', numElem * #lines)	-- allocate space for xyz of each planet 
 
 local maxAbs = 0
 for i,line in ipairs(lines) do
@@ -17,17 +18,19 @@ for i,line in ipairs(lines) do
 	local x = assert(tonumber(parts[columnForNames.X]))
 	local y = assert(tonumber(parts[columnForNames.Y]))
 	local z = assert(tonumber(parts[columnForNames.Z]))
-	buffer[0+3*(i-1)] = x
-	buffer[1+3*(i-1)] = y
-	buffer[2+3*(i-1)] = z
-	maxAbs = math.max(maxAbs,x)
-	maxAbs = math.max(maxAbs,y)
-	maxAbs = math.max(maxAbs,z)
+	local mag = assert(tonumber(parts[columnForNames.AbsMag]))	--apparent magnitude
+	buffer[0 + numElem * (i - 1)] = x
+	buffer[1 + numElem * (i - 1)] = y
+	buffer[2 + numElem * (i - 1)] = z
+	buffer[3 + numElem * (i - 1)] = mag
+	maxAbs = math.max(maxAbs, x)
+	maxAbs = math.max(maxAbs, y)
+	maxAbs = math.max(maxAbs, z)
 end
 print('abs max coordinate',maxAbs)
 
-local filename = 'starpositions.f32'
+local filename = 'stardata.f32'
 local fh = ffi.C.fopen(filename, 'wb') or error("failed to open file "..filename.." for writing")
-if ffi.C.fwrite(buffer, ffi.sizeof('float') * 3 * #lines, 1, fh) ~= 1 then error("failed to write data to "..filename) end
+if ffi.C.fwrite(buffer, ffi.sizeof('float') * numElem * #lines, 1, fh) ~= 1 then error("failed to write data to "..filename) end
 ffi.C.fclose(fh)
 
