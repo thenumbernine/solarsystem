c9c9b4effe52f84636d07adebc628ace4c69e371
thenumbernine <christopher.e.moore@gmail.com>
2022-09-16 01:21:40 +0700

    updating to new ext.file

--- exoplanet/parseCaltech.lua
+++ exoplanet/parseCaltech.lua
@@ -234,4 +234,4 @@ end
 print('max angle error (deg)', math.deg(maxAngleError))
 print("didn't find distance for "..didntFindDistance.." of "..(foundDistance + didntFindDistance).." stars")
 
-io.writefile('exoplanetResults.json', 'exoplanetInfo = \n' .. json.encode(results, {indent=true}) .. '\n;')
+file'exoplanetResults.json':write('exoplanetInfo = \n' .. json.encode(results, {indent=true}) .. '\n;')
