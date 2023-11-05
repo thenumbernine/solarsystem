59d79dbbb7566a127d1da449119fe7a4ec951680
Christopher Moore <christopher.e.moore@gmail.com>
2023-11-05 09:31:04 +0700

    more

--- offline/compare-gaiadr2-vs-hyg.lua
+++ offline/compare-gaiadr2-vs-hyg.lua
@@ -11,8 +11,8 @@ summary of results:
 119614 stars in HYG
 117956 stars in HYG and Hipparcos
 83034 stars in Gaia-DR2 and Hipparcos
-83033 stars in Gaia-DR2, Hipparcos, and HYG 
-(Looks like only Hipparcos #54976 is missing from HYG ... 
+83033 stars in Gaia-DR2, Hipparcos, and HYG
+(Looks like only Hipparcos #54976 is missing from HYG ...
  I hope I didn't delete that line accidentally)
 145 named stars in HYG and Hipparcos
 46 named stars in Gai-DR2, Hipparcos, and HYG
@@ -20,15 +20,15 @@ summary of results:
 now what else is interesting is that the log10lum range of hyg is much dimmer than the log10lum range of gaia-dr2
 gaia-dr2 seems to cut off just below -1.5
 
-i wonder if the luminosity data between hyg and gaia compares?
+I wonder if the luminosity data between hyg and gaia compares?
 --]]
 local table = require 'ext.table'
 local CSV = require 'csv'
 
--- TODO HERE 
+-- TODO HERE
 -- load the gaia-dr2 fits file
 -- and extract the rows in it associated with (named/all) hipparcos stars
--- and compare stats 
+-- and compare stats
 
 print'reading hyg'
 local hyg = CSV.file'../hyg/hyg_v37.csv'
@@ -38,21 +38,21 @@ print'building hyg lookup for hip'
 local hygForHip = table()
 local hygForHipCount = 0
 for _,row in ipairs(hyg.rows) do
-	if row.hip ~= '' 
+	if row.hip ~= ''
 	and row.proper ~= ''
 	then
 		hygForHip[tonumber(row.hip)] = row
 		hygForHipCount = hygForHipCount + 1
 	end
 end
-print('found '..hygForHipCount..' named stars shared between the hipparcos and hyg databases') 
+print('found '..hygForHipCount..' named stars shared between the hipparcos and hyg databases')
 
 print'reading gaia-cross-hipparcos'
 local gaia = CSV.file'gaiadr2.hipparcos2_best_neighbour.csv'
 gaia:setColumnNames(gaia.rows:remove(1))
 
 -- for each gaia-cross row find the hyg row
--- and then find 
+-- and then find
 print'searching'
 gaia_hyg_count = 0
 for _,gaiarow in ipairs(gaia.rows) do
@@ -60,7 +60,7 @@ for _,gaiarow in ipairs(gaia.rows) do
 	hip = assert(tonumber(hip), "failed to convert hipparcos id "..hip.." to number")
 	-- looks like not all hipparcos stars (such as #54976) are in the hyg database
 	local hygrow = hygForHip[hip]
-	if hygrow 
+	if hygrow
 	then
 		gaia_hyg_count = gaia_hyg_count + 1
 		print(hygrow.proper, hygrow.hip, gaiarow.source_id)
